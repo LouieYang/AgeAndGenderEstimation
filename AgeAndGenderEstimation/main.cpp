65017c8727e02b76770d48df7bcc2473acb5de0f
LiuYang <651636074@qq.com>
2016-02-13 20:31:38 +0800

    Initial Commit

---
+++ AgeAndGenderEstimation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AgeAndGenderEstimation
+//
+//  Created by 刘洋 on 16/2/13.
+//  Copyright © 2016年 Louie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
